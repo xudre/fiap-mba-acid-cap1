1d656ba05e7d54e26973a181c812f56dfa6b9fb0
Pedro Xudre <pedroxudre@gmail.com>
2022-04-13 19:55:52 -0300

    - Firebase setup - Crashlytics test

--- app/src/main/java/br/com/fiap/acid/cap1/MainActivity.kt
+++ app/src/main/java/br/com/fiap/acid/cap1/MainActivity.kt
@@ -1,11 +1,27 @@
 package br.com.fiap.acid.cap1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        setupContent()
+    }
+
+    private fun setupContent() {
+        val btnAction = findViewById<Button>(R.id.btn_action)
+
+        btnAction.setOnClickListener {
+            doCrashAction()
+        }
+    }
+
+    private fun doCrashAction() {
+        throw RuntimeException("Crashou!")
     }
 }
