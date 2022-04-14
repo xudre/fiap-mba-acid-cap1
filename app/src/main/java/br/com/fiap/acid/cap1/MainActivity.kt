0092d6ddc032db4a4bd4e7b65e74ace15a2eb5da
Pedro Xudre <pedro@xudre.com>
2022-04-13 22:00:40 -0300

    - Crashlytics included - Analytics event fire

--- app/src/main/java/br/com/fiap/acid/cap1/MainActivity.kt
+++ app/src/main/java/br/com/fiap/acid/cap1/MainActivity.kt
@@ -3,25 +3,41 @@ package br.com.fiap.acid.cap1
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
         setupContent()
     }
 
     private fun setupContent() {
+        val btnCrash = findViewById<Button>(R.id.btn_crash)
         val btnAction = findViewById<Button>(R.id.btn_action)
 
-        btnAction.setOnClickListener {
+        btnCrash.setOnClickListener {
             doCrashAction()
+        }
+
+        btnAction.setOnClickListener {
+            doAnalyticsEvent("Click")
         }
     }
 
     private fun doCrashAction() {
         throw RuntimeException("Crashou!")
+    }
+
+    private fun doAnalyticsEvent(event: String) {
+        firebaseAnalytics.logEvent("Evento_$event", bundleOf())
     }
 }
